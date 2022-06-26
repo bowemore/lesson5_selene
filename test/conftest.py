20e0ab1df5747d5b035904b6fbbbf0a2539cadbb
walle <walle@123.ru>
2022-06-26 15:09:26 +0300

    added homework

---
+++ test/conftest.py
@@ -0,0 +1,9 @@
+from selene.support.shared import browser
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.hold_browser_open = True
+
---
+++ test/test_practice_form.py
@@ -0,0 +1,73 @@
+from selene import have
+from selene.support.shared import browser
+import calendar
+
+firstName = 'Mike'
+lastName = 'Tyson'
+userEmail = 'mike@tyson.com'
+gender = 'Male'
+userNumber = '9876543210'
+dayOfBirth = "30th"
+mothOfBirth = "6"
+mothOfBirthName = calendar.month_name[int(mothOfBirth)]
+yearOfBirth = '1966'
+subject = 'English'
+hobbies = 'Sports'
+currentAddress = "10250 Constellation Blvd., 9th Floor, Los Angeles"
+uploadPicture = r'C:\Users\WALLE\PycharmProjects\lesson5_selene\test\1.png'
+state = 'NCR'
+city = 'Delhi'
+
+
+def test_submit_form():
+    browser.open('/automation-practice-form')
+    browser.element('#firstName').type(firstName)
+    browser.element('#lastName').type(lastName)
+    browser.element('#userEmail').type(userEmail)
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type(userNumber)
+    browser.element('#dateOfBirthInput').click()
+    browser.element(f'[value="{yearOfBirth}"]').click()
+    browser.element(f'[value="{mothOfBirth}"]').click()
+    browser.element(f'div[aria-label="Choose Thursday, {mothOfBirthName} {dayOfBirth}, {yearOfBirth}"]').click()
+    browser.element('#subjectsInput').type(subject).press_enter()
+    browser.element('[for=hobbies-checkbox-1]').click()
+    browser.element('#uploadPicture').type(uploadPicture)
+    browser.element('#currentAddress').type(currentAddress).press_tab()
+    browser.element('#state input').type(state).press_tab()
+    browser.element('#city input').type(city).press_enter()
+    browser.element('#submit').click()
+
+    browser.all('table tr').should(have.exact_texts(
+        'Label Values',
+        f'Student Name {firstName} {lastName}',
+        f'Student Email {userEmail}',
+        f'Gender {gender}',
+        f'Mobile {userNumber}',
+        f'Date of Birth {dayOfBirth[:-2]} {mothOfBirthName},{yearOfBirth}',
+        f'Subjects {subject}',
+        f'Hobbies {hobbies}',
+        f'Picture {uploadPicture[-5:]}',
+        f'Address {currentAddress}',
+        f'State and City {state} {city}'
+    ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
