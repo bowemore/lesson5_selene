0121be5aa50d35180af92735cb30cf3fb25b8b4b
walle <walle@123.ru>
2022-07-03 15:09:58 +0300

    added browser size

--- test/conftest.py
+++ test/conftest.py
@@ -6,4 +6,8 @@ import pytest
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.hold_browser_open = True
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+
+
 
